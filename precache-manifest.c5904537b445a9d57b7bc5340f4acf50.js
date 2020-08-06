4b757e342095245ddf604c2c13fad4cc80a39fc3
zhiyangcai <2313120974@qq.com>
2020-08-06 15:34:53 +0800

    更新保存操作

---
+++ precache-manifest.c5904537b445a9d57b7bc5340f4acf50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22acded7359aa1383759",
+    "url": "/morney-website/css/app.701f134f.css"
+  },
+  {
+    "revision": "9aedd255a809e18e6c7ce6fecdeaa066",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "22acded7359aa1383759",
+    "url": "/morney-website/js/app.0d525aac.js"
+  },
+  {
+    "revision": "994fcb6d39c85d649734",
+    "url": "/morney-website/js/chunk-vendors.1e989364.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "d6f1d6421da4b19af397e5a8bfd2687c",
+    "url": "/morney-website/morneyCode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
